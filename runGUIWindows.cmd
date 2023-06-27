7be6d59f3a805d8d1960ca69961a2e7656f0e558
Saalim Saadman Araf <saalim.araf@gmail.com>
2023-06-27 14:35:13 +0600

    naming changes

--- runGUIWindows.cmd
+++ runGUIWindows.cmd
@@ -1,1 +1,1 @@
-java --module-path javafx-sdk-20.0.1/lib --add-modules javafx.controls,javafx.fxml -jar .\GalacticStore.jar
+java --module-path javafx-sdk-20.0.1/lib --add-modules javafx.controls,javafx.fxml -jar .\Galactic-Store-GUI.jar
